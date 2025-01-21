0be8a8d0886ea566b5d22c00c23a77be99f084c1
ReaperBecca <122236852+ReaperBecca@users.noreply.github.com>
2025-01-21 13:58:52 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,5 @@ const { initializeAppData, syncDefaultThemes } = require('./preloader');
 app.on('ready', () => {
     initializeAppData();
     syncDefaultThemes();
-    windowManager.createMainWindow('https://raw.githubusercontent.com/ReaperBecca/rmi_client_git/refs/heads/main/Pages/home.html');
-
-    // remove 'https://raw.githubusercontent.com/ReaperBecca/rmi_client_git/refs/heads/main/Home/home.js' to open as a blank page
+    windowManager.createMainWindow('https://raw.githubusercontent.com/ReaperBecca/rmi_client_git/refs/heads/main/navi.html');
 });
