5efc01488d6b636aa5118f8e0048b16513cfe20b
ReaperBecca <122236852+ReaperBecca@users.noreply.github.com>
2025-01-22 17:26:43 -0500

    Update games.js

--- Pages/games.js
+++ Pages/games.js
@@ -128,24 +128,6 @@ function addGameClickHandlers() {
     });
 }
 
-function setupNavigation() {
-    const buttons = {
-        homeButton: '../Home/home.html',
-        gamesButton: '../Games/games.html',
-        aboutButton: '../About/about.html',
-        accountButton: '../Account/account.html'
-    };
-
-    Object.entries(buttons).forEach(([buttonId, path]) => {
-        const button = document.getElementById(buttonId);
-        if (button) {
-            button.addEventListener('click', () => {
-                window.location.href = path;
-            });
-        }
-    });
-}
-
 function filterGamesByType(games, type) {
     return games.filter(game => game.directoryType === type);
 }
