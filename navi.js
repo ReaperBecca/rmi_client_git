1fc6822d6827fdbc9775767e8b95f0a01627cf5d
ReaperBecca <122236852+ReaperBecca@users.noreply.github.com>
2025-01-21 21:36:12 -0500

    Update navi.js

--- navi.js
+++ navi.js
@@ -13,7 +13,7 @@ class Navigation {
     loadNavigationStyles() {
         const link = document.createElement('link');
         link.rel = 'stylesheet';
-        link.href = './Themes/Reaper\'sDarkMode/navi.css';
+        link.href = '/Themes/Reaper\'sDarkMode/navi.css';
         document.head.appendChild(link);
     }
 
