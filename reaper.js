9447e6d609daa62bafcef77db27cc732c294ca3d
ReaperBecca <122236852+ReaperBecca@users.noreply.github.com>
2025-01-24 17:11:27 -0500

    Update reaper.js

--- reaper.js
+++ reaper.js
@@ -3,17 +3,28 @@ export function handleReaperUrl(url) {
     if (url.startsWith('Reaper://')) {
         const path = url.substring('Reaper://'.length);
         
+        console.error('Reaper URL: ${url}');
         console.log('Reaper URL path: ${path}');
 
-        const urlMappings = {
-            'Media.ind/Home': './Pages/home.html',
-            'Media.ind/Games': './Pages/games.html',
-            'Media.ind/About': './Pages/about.html',
-            'Media.ind/Account': './Pages/account.html'
-        };
-
-        return urlMappings[path]
+        return null;
     }
 
+    const path = url.substring('Reaper://'.length);
+
+    console.log('Reaper URL: ${path}');
+
+    const urlMappings = {
+        'Media.ind/Home': './Pages/home.html',
+        'Media.ind/Games': './Pages/games.html',
+        'Media.ind/About': './Pages/about.html',
+        'Media.ind/Account': './Pages/account.html'
+    };
+
+    if (urlMappings[path]) {
+        return urlMappings[path];
+    } 
+
+    console.warn('Unhandled Reaper URL: ${path}');
     return null;
+
 }
