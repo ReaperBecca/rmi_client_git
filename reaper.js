1d4234faa18e67f5bf54ea2302d3230eed4d442a
ReaperBecca <122236852+ReaperBecca@users.noreply.github.com>
2025-01-28 15:36:03 -0500

    Update reaper.js

--- reaper.js
+++ reaper.js
@@ -1,29 +1,24 @@
 export class ReaperNavigator {
     constructor() {
-        this.urlMappings = {
-            'Media.ind/Home': './Pages/home.html',
-            'Media.ind/Games': './Pages/games.html', 
-            'Media.ind/About': './Pages/about.html',
-            'Media.ind/Account': './Pages/account.html'
-        };
+        this.baseUrl = 'https://reaperbecca.github.io/rmi_client_git/';
+        this.reaperPrefix = 'Reaper://Media.ind/';
     }
 
     handleReaperUrl(url) {
-        if (!url.startsWith('Reaper://')) {
+        if (!url.startsWith(this.reaperPrefix)) {
             return null;
         }
 
-        const path = url.substring('Reaper://'.length);
+        const path = url.substring(this.reaperPrefix.length);
         console.log(`Navigating to: ${path}`);
-
-        return this.urlMappings[path] || null;
+        return this.baseUrl + path;
     }
 
     validateUrl(url) {
-        return url.startsWith('Reaper://') && this.urlMappings[url.substring('Reaper://'.length)];
+        return url.startsWith(this.reaperPrefix);
     }
 
     getDefaultUrl() {
-        return 'Reaper://Media.ind/Home';
+        return this.reaperPrefix + 'Home';
     }
 }
