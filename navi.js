33f34632348b5425347bf8a81e5dd1a8e72a815d
ReaperBecca <122236852+ReaperBecca@users.noreply.github.com>
2025-01-28 15:43:26 -0500

    Update navi.js

--- navi.js
+++ navi.js
@@ -3,52 +3,14 @@ import { ReaperNavigator } from "./reaper";
 class Navigation {
     constructor() {
         this.reaperNavigator = new ReaperNavigator();
-        this.buttons = {
-            homeButton: 'Reaper://Media.ind/Home',
-            gamesButton: 'Reaper://Media.ind/Games',
-            aboutButton: 'Reaper://Media.ind/About',
-            accountButton: 'Reaper://Media.ind/Account'
-        };
-        
         this.contentFrame = document.getElementById('contentFrame');
         this.omniboxInput = document.getElementById('omniboxInput');
-        
-        this.initializeNavigation();
 
         const defaultUrl = this.reaperNavigator.getDefaultUrl();
         this.omniboxInput.value = defaultUrl;
         this.updateContentFrame(defaultUrl);
-        this.setActiveButton('homeButton');
 
         this.omniboxInput.addEventListener('input', this.handleOmniboxInput.bind(this));
-    }
-
-    initializeNavigation() {
-        Object.entries(this.buttons).forEach(([buttonId, reaperUrl]) => {
-            const button = document.getElementById(buttonId);
-            if (button) {
-                button.addEventListener('click', () => {
-                    this.setActiveButton(buttonId);
-                    this.omniboxInput.value = reaperUrl;
-                    this.updateContentFrame(reaperUrl);
-                });
-            }
-        });
-    }
-
-    setActiveButton(activeId) {
-        // Remove 'active' class from all buttons
-        Object.keys(this.buttons).forEach(buttonId => {
-            const button = document.getElementById(buttonId);
-            if (button) {
-                button.classList.remove('active');
-            }
-        });
-        // Add active class to clicked button
-        const activeButton = document.getElementById(activeId);
-        if (activeButton) {
-            activeButton.classList.add('active');
-        }
     }
 
     handleOmniboxInput(event) {
@@ -57,12 +19,6 @@ class Navigation {
         
         if (result) {
             this.updateContentFrame(url);
-            const activeButton = Object.entries(this.buttons)
-                .find(([_, value]) => value === url)?.[0];
-            
-            if (activeButton) {
-                this.setActiveButton(activeButton);
-            }
         }
     }
 
@@ -73,7 +29,6 @@ class Navigation {
         }
     }
 }
-
 
 document.addEventListener('DOMContentLoaded', () => {
     new Navigation();
