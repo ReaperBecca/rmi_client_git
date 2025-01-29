41aa15e8d3cd7bc093d8b7a9b96226409d8921c1
ReaperBecca <122236852+ReaperBecca@users.noreply.github.com>
2025-01-29 13:17:31 -0500

    summary

--- main.js
+++
@@ -1,9 +0,0 @@
-const { app } = require('electron');
-const { windowManager } = require('./window');
-const { initializeAppData, syncDefaultThemes } = require('./preloader');
-
-app.on('ready', () => {
-    initializeAppData();
-    syncDefaultThemes();
-    windowManager.createMainWindow();
-});
